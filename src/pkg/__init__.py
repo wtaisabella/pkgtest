de642299a1ae57cad14d84422f93a9ed6197faaf
wtaisabella <yuting.lu1994@gmail.com>
2022-04-27 13:42:20 +0900

    Update __init__.py

--- src/pkg/__init__.py
+++ src/pkg/__init__.py
@@ -5,3 +5,15 @@ Created on Wed Apr 27 12:56:44 2022
 @author: LYT
 """
 __all__=['my_module']
+
+__version__ = "0.0.1"
+__uri__ = "none"
+__author__ = "Yuting Lu"
+__email__ = ""
+__license__ = "MIT"
+__description__ = "package test"
+
+#from . import lc_analysis
+#from . import tpf_analysis
+#from . import utils
+#from . import ffi_analysis
